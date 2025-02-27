6ff9541af508245b2fc60e1b47e56ecb8232bdd6
ingmuyleang <muyleanging@gmail.com>
2025-02-27 19:47:20 +0700

    asdfg

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    // ... any other existing configurations
+}
+
+module.exports = nextConfig
+
